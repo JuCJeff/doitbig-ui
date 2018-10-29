b5a1ac10340615cc16b88734c310539a129c8f17
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-10-29 11:21:51 -0500

    Implemented the signup route

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -16,12 +16,14 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import SignupPage from '../SignupPage';
 
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/signup" component={SignupPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
