4dd21b328652e9333ae0dc329c15685eafe28af7
Jeff Ma <zma67@wisc.edu>
2018-11-01 20:50:45 -0500

    Added sign in page UI structs

--- app/containers/SignInPage/SignInForm.js
+++ app/containers/SignInPage/SignInForm.js
@@ -1,28 +1,38 @@
 import React from 'react';
+// import { TransitionMotion, spring } from 'react-motion';
 import './SignInForm.css';
 
 /* eslint-disable react/prefer-stateless-function */
-class SignInForm extends React.Component {
+export default class SignInForm extends React.PureComponent {
+  // constructor() {
+  //   super();
+
+  //   this.state = {
+  //     inputValue: '',
+  //     fieldActive: false,
+  //   };
+  // }
+
   render() {
     return (
-      <form>
-        <h2>Login</h2>
-        <div className="inputBox">
-          <label htmlFor="username">
+      <div className="box">
+        <form>
+          <h2>STS Student Login</h2>
+          <div className="inputBox">
+            {/* <label htmlFor="username"> */}
             <input type="text" name="username" required="" />
             Username
-          </label>
-        </div>
-        <div className="inputBox">
-          <label htmlFor="password">
+            {/* </label> */}
+          </div>
+          <div className="inputBox">
+            {/* <label htmlFor="password"> */}
             <input type="password" name="password" required="" />
             Password
-          </label>
-        </div>
-        <input type="submit" name="submit" value="Submit" />
-      </form>
+            {/* </label> */}
+          </div>
+          <input type="submit" name="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
-
-export default SignInForm;
--- app/containers/SignInPage/index.js
+++ app/containers/SignInPage/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SignInForm from './SignInForm';
+import Background from '../../images/Background.png';
+import './index.css';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class SignInPage extends React.PureComponent {
   render() {
     return (
-      <div className="box">
-        <h2>Login</h2>
+      <div className="body">
+        <img src={Background} alt="background" />
         <SignInForm />
       </div>
     );
