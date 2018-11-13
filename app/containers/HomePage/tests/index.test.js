272c37bc331f45680c48b82fa27c9882b073a3b9
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-13 17:29:06 -0600

    tests

--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 import { shallow } from 'enzyme';
+import Grid from '@material-ui/core/Grid';
+import Tracks from '../Tracks';
 
 import HomePage from '../index';
-import messages from '../messages';
 
 describe('<HomePage />', () => {
   it('should render the page message', () => {
     const renderedComponent = shallow(<HomePage />);
     expect(
-      renderedComponent.contains(<FormattedMessage {...messages.header} />),
-    ).toEqual(true);
+      renderedComponent.contains(
+        <Grid item sm={4}>
+          <Tracks />
+        </Grid>,
+      ),
+    ).toEqual(false);
   });
 });
