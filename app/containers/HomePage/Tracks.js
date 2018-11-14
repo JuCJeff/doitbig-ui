54108b5a288b94830933b459c32def6ee7dfa56f
Jeff Ma <zma67@wisc.edu>
2018-11-13 23:02:41 -0600

    Added banner image

--- app/containers/HomePage/Tracks.js
+++ app/containers/HomePage/Tracks.js
@@ -8,7 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Image from '../../images/STS.jpg';
+import Image from '../../images/STS-Banner.jpg';
 
 const styles = {
   card: {
