c22b4da43c22c6f1edb83bc1872d54784ce9d100
Jeff Ma <zma67@wisc.edu>
2018-11-13 23:25:37 -0600

    Added animation APIs

---
+++ app/containers/TracksPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class TracksPage extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>Hello World</h2>
+      </div>
+    );
+  }
+}
