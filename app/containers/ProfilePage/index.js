d0301aadd029e72ac6ac0f253aaf3a7d7a246c12
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-13 20:53:36 -0600

    Rough connection between ui and ms

--- app/containers/ProfilePage/index.js
+++ app/containers/ProfilePage/index.js
@@ -11,77 +11,123 @@
 
 import React from 'react';
 import './index.css';
+const axios = require('axios');
+
+const server = axios.create({
+  baseURL: 'http://localhost:3001',
+});
 
 /* eslint-disable react/prefer-stateless-function */
 export default class ProfilePage extends React.PureComponent {
-  render() {
-    return (
-      <div className="box">
-        <h1>Profile</h1>
-        <img
-          /* eslint-disable global-require */
-          src={require('../../images/nobody_m.original.jpg')}
-          /* eslint-disable global-require */
-          alt="profile"
-          width="150px"
-        />
-        <h2>Name of User</h2>
-        {/* any image for profile instead of button */}
-        <ul>
-          <li>
-            <strong>First Name: </strong>
-          </li>
-          <li>
-            <strong>Last Name: </strong>
-          </li>
-          <li>
-            <strong>Email: </strong>
-          </li>
-          <li>
-            <strong>Previous Password: </strong>
-          </li>
-          <li>
-            <strong>New Password: </strong>
-          </li>
-          <li>
-            <strong>Confirm Password: </strong>
-          </li>
-          <div className="courses">
-            <li>
-              <strong>Number of Courses Taken: </strong>
-            </li>
-            <li>
-              <strong>Name of Courses Taken: </strong>
-            </li>
-            <table>
-              <tr>
-                <th>Class Name</th>
-                <th>Instructor</th>
-                <th>Track</th>
-                <th>Date</th>
-              </tr>
-              <tr>
-                <td>Class 1</td>
-                <td>...</td>
-                <td>...</td>
-              </tr>
-              <tr>
-                <td>Class 2</td>
-                <td>...</td>
-                <td>...</td>
-              </tr>
-              <tr>
-                <td>Class 3</td>
-                <td>...</td>
-                <td>...</td>
-              </tr>
-            </table>
-            <li>
-              <strong>Upcoming: </strong>
-            </li>
-          </div>
-        </ul>
-      </div>
+  // async getUser(id) {
+  //   try {
+  //     const response = await server.get(`/profile?uid=${id}`);
+  //     console.log(response.data);
+  //     return response.data;
+  //   } catch (error) {
+  //     return error;
+  //   }
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    server.get(`/profile?uid=12345`).then(
+      result => {
+        this.setState({
+          isLoaded: true,
+          items: result.data,
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      error => {
+        this.setState({
+          isLoaded: true,
+          error,
+        });
+      },
     );
+  }
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (!error && isLoaded) {
+      return (
+        <div className="box">
+          <h1>Profile</h1>
+          <img
+            /* eslint-disable global-require */
+            src={require('../../images/nobody_m.original.jpg')}
+            /* eslint-disable global-require */
+            alt="profile"
+            width="150px"
+          />
+          <h2>Name of User</h2>
+          {/* any image for profile instead of button */}
+          <ul>
+            <li>
+              <strong>First Name: {items.FirstName}</strong>
+            </li>
+            <li>
+              <strong>Last Name: {items.LastName}</strong>
+            </li>
+            <li>
+              <strong>Email: {items.Email}</strong>
+            </li>
+            <li>
+              <strong>Previous Password: </strong>
+            </li>
+            <li>
+              <strong>New Password: </strong>
+            </li>
+            <li>
+              <strong>Confirm Password: </strong>
+            </li>
+            <div className="courses">
+              <li>
+                <strong>Number of Courses Taken: </strong>
+              </li>
+              <li>
+                <strong>Name of Courses Taken: </strong>
+              </li>
+              <table>
+                <tr>
+                  <th>Class Name</th>
+                  <th>Instructor</th>
+                  <th>Track</th>
+                  <th>Date</th>
+                </tr>
+                <tr>
+                  <td>Class 1</td>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <td>Class 2</td>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+                <tr>
+                  <td>Class 3</td>
+                  <td>...</td>
+                  <td>...</td>
+                </tr>
+              </table>
+              <li>
+                <strong>Upcoming: </strong>
+              </li>
+            </div>
+          </ul>
+        </div>
+      );
+    }
+    return <p>Loading...</p>;
   }
 }
