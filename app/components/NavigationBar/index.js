c6b66310459f569372ac1b15be0e01fb6a247a4e
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-16 19:06:09 -0600

    Fixed stuff

---
+++ app/components/NavigationBar/index.js
@@ -0,0 +1,60 @@
+/*
+ * navigationBar
+ *
+ * This is the navigation bar of our App, at the top of each page.
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a necessity for you then you can refactor it and remove
+ * the linting exception.
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import { Avatar } from '@material-ui/core';
+import STS from '../../images/STS.png';
+import MenuListComposition from '../Dropdown/index';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function ButtonAppBar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            <Button color="inherit">
+              <Avatar src={STS} />Software Training for Students
+            </Button>
+          </Typography>
+          <Button color="inherit">Tracks</Button>
+          <Button color="inherit">Courses</Button>
+          <MenuListComposition />
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
