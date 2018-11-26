ff39eab0114395d96a720c3974d9a4a47b55ef6a
Jeff Ma <zma67@wisc.edu>
2018-11-26 15:33:28 -0600

    Added navigation bar and banner

--- app/containers/CoursePage/index.js
+++ app/containers/CoursePage/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './index.css';
+import NavigationBar from '../../components/NavigationBar/loadable';
+import Image from '../../images/STS.jpg';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class NotFound extends React.PureComponent {
   render() {
-    return <p>Course Page</p>;
+    return (
+      <div>
+        <div>
+          <NavigationBar />
+        </div>
+        <img src={Image} className="images" alt="banner" />
+      </div>
+    );
   }
 }
