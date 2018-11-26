51f8a47067a559b338dfb042c61d2cbbbdccaf8e
Jeff Ma <zma67@wisc.edu>
2018-11-26 15:28:04 -0600

    Added course page route

---
+++ app/containers/CoursePage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+
+/* eslint-disable react/prefer-stateless-function */
+export default class NotFound extends React.PureComponent {
+  render() {
+    return <p>Course Page</p>;
+  }
+}
