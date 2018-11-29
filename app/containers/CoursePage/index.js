6d0c8f11bfbdbe0d55546d568cdf2c0aebfee749
Jeff Ma <zma67@wisc.edu>
2018-11-29 17:23:23 -0600

    Updated structs in course page

--- app/containers/CoursePage/index.js
+++ app/containers/CoursePage/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Description from './description';
+import Avatar from './avatar';
 import './index.css';
 import NavigationBar from '../../components/NavigationBar/loadable';
 import Image from '../../images/STS.jpg';
@@ -12,6 +14,10 @@ export default class NotFound extends React.PureComponent {
           <NavigationBar />
         </div>
         <img src={Image} className="images" alt="banner" />
+        <div className="description">
+          <Description />
+        </div>
+        <Avatar />
       </div>
     );
   }
