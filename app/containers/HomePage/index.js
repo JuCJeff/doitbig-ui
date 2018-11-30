119c873605ce32e96cc6227930c5048af69bbf79
hyang344 <hyang344@wisc.edu>
2018-11-29 19:04:44 -0600

    push again

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,18 +1,5 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
-
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+// import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 
 import NavigationBar from '../../components/NavigationBar/index';
@@ -21,68 +8,101 @@ import Courses from './Courses';
 import Image from '../../images/STS.jpg';
 import './style.css';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  tracks: {
-    padding: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-});
+export default class HomePage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      track_1: '1',
+      track_1_des: '1',
+      track_2: '1',
+      track_2_des: '1',
+      track_3: '1',
+      track_3_des: '1',
+      course_1: '1',
+      course_1_des: '1',
+      course_2: '1',
+      course_2_des: '1',
+      course_3: '1',
+      course_3_des: 'courseThreeDes',
+    };
+  }
 
-// /* eslint-disable react/prefer-stateless-function */
-// export default class HomePage extends React.PureComponent {
-//   render() {
-//     return (
-//       <div>
-//         <img src={Image} class="images" />
-//         <Tracks />
-//         <Courses />
-//       </div>
-//     );
-//   }
-// }
+  hello() {
+    fetch('http://localhost:3001/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ track_1: responseJson.track_1 });
+        this.setState({ track_2: responseJson.track_2 });
+        this.setState({ track_3: responseJson.track_3 });
+        this.setState({ track_1_des: responseJson.track_1_des });
+        this.setState({ track_2_des: responseJson.track_2_des });
+        this.setState({ track_3_des: responseJson.track_3_des });
+        this.setState({ course_1: responseJson.course_1 });
+        this.setState({ course_2: responseJson.course_2 });
+        this.setState({ course_3: responseJson.course_3 });
+        this.setState({ course_1_des: responseJson.course_1_des });
+        this.setState({ course_2_des: responseJson.course_2_des });
+        this.setState({ course_3_des: responseJson.course_3_des });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-function HomePage(props) {
-  const { classes } = props;
-
-  return (
-    <div className={classes.root}>
+  render() {
+    this.hello();
+    return (
       <div>
-        <NavigationBar />
-      </div>
-      <img src={Image} className="images" alt="banner" />
-      <Grid container spacing={24} align="center">
-        <Grid item sm={4}>
-          <Tracks />
+        {this.state.track_1}
+        {this.state.track_2}
+        {this.state.track_3}
+        {this.state.track_1_des}
+        {this.state.track_2_des}
+        {this.state.track_3_des}
+        {this.state.course_1}
+        {this.state.course_2}
+        {this.state.course_3}
+        {this.state.course_1_des}
+        {this.state.course_2_des}
+        {this.state.course_3_des}
+        <div>
+          <NavigationBar />
+        </div>
+        <img src={Image} className="images" alt="banner" />
+        <Grid container spacing={24} align="center">
+          <Grid item sm={4}>
+            <Tracks />
+          </Grid>
+          <Grid item sm={4}>
+            <Tracks />
+          </Grid>
+          <Grid item sm={4}>
+            <Tracks />
+          </Grid>
         </Grid>
-        <Grid item sm={4}>
-          <Tracks />
-        </Grid>
-        <Grid item sm={4}>
-          <Tracks />
-        </Grid>
-      </Grid>
 
-      <Grid container spacing={24}>
-        <Grid item xs={12}>
-          <Courses />
+        <Grid container spacing={24}>
+          <Grid item xs={12}>
+            <Courses />
+          </Grid>
+          <Grid item xs={12}>
+            <Courses />
+          </Grid>
+          <Grid item xs={12}>
+            <Courses />
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <Courses />
-        </Grid>
-        <Grid item xs={12}>
-          <Courses />
-        </Grid>
-      </Grid>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-HomePage.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(HomePage);
+// HomePage.propTypes = {
+//  classes: PropTypes.object.isRequired,
+// };
