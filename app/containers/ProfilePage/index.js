444d45f6d6db98c572b0ad0872360e2f9332b10e
Jeff Ma <zma67@wisc.edu>
2018-11-30 01:13:23 -0600

    Did basic styling to profile page

--- app/containers/ProfilePage/index.js
+++ app/containers/ProfilePage/index.js
@@ -12,6 +12,8 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import './index.css';
 import NavigationBar from '../../components/NavigationBar/index';
+import Image from '../../images/nobody_m.original.jpg';
+
 const axios = require('axios');
 const qs = require('query-string');
 const server = axios.create({
@@ -50,71 +52,20 @@ export default class ProfilePage extends React.PureComponent {
     const { error, isLoaded, items } = this.state;
     if (!error && isLoaded) {
       return (
-        <div className="box">
+        <div className="body">
           <NavigationBar />
-          <h1>Profile</h1>
-          <img
-            /* eslint-disable global-require */
-            src={require('../../images/nobody_m.original.jpg')}
-            /* eslint-disable global-require */
-            alt="profile"
-            width="150px"
-          />
-          <h2>Name of User</h2>
-          {/* any image for profile instead of button */}
+          <img src={Image} alt="profile" width="150px" className="images" />
+
           <ul>
-            <li>
-              <strong>First Name: {items.FirstName}</strong>
-            </li>
-            <li>
-              <strong>Last Name: {items.LastName}</strong>
-            </li>
-            <li>
-              <strong>Email: {items.Email}</strong>
-            </li>
-            <li>
-              <strong>Previous Password: </strong>
-            </li>
-            <li>
-              <strong>New Password: </strong>
-            </li>
-            <li>
-              <strong>Confirm Password: </strong>
-            </li>
-            <div className="courses">
-              <li>
-                <strong>Number of Courses Taken: </strong>
-              </li>
-              <li>
-                <strong>Name of Courses Taken: </strong>
-              </li>
-              <table>
-                <tr>
-                  <th>Class Name</th>
-                  <th>Instructor</th>
-                  <th>Track</th>
-                  <th>Date</th>
-                </tr>
-                <tr>
-                  <td>Class 1</td>
-                  <td>...</td>
-                  <td>...</td>
-                </tr>
-                <tr>
-                  <td>Class 2</td>
-                  <td>...</td>
-                  <td>...</td>
-                </tr>
-                <tr>
-                  <td>Class 3</td>
-                  <td>...</td>
-                  <td>...</td>
-                </tr>
-              </table>
-              <li>
-                <strong>Upcoming: </strong>
-              </li>
-            </div>
+            <li>First Name: {items.FirstName}</li>
+            <li>Last Name: {items.LastName}</li>
+            <li>Email: {items.Email}</li>
+          </ul>
+
+          <ul className="courses">
+            <li># of Courses Taken:</li>
+            <li># of Courses Taken:</li>
+            <li>Upcoming course:</li>
           </ul>
         </div>
       );
