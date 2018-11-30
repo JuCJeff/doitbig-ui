01a03d228bb870830725c55c854052c130148bd7
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-30 14:08:02 -0600

    Added login functionality

--- app/containers/SignInPage/SignInForm.js
+++ app/containers/SignInPage/SignInForm.js
@@ -1,30 +1,20 @@
 import React from 'react';
-// import { TransitionMotion, spring } from 'react-motion';
 import './SignInForm.css';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class SignInForm extends React.PureComponent {
-  // constructor() {
-  //   super();
-
-  //   this.state = {
-  //     inputValue: '',
-  //     fieldActive: false,
-  //   };
-  // }
-
   render() {
     return (
       <div className="box">
         <h1 className="title_h1">STS STUDENT</h1>
         <h4 className="title_h4">Please sign in to your account.</h4>
 
-        <form>
+        <form action="http://localhost:3001/login" method="get">
           <div className="signinRow">
-            <input type="email" placeholder="Email" />
+            <input type="email" placeholder="Email" name="email" />
           </div>
           <div className="signinRow">
-            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Password" name="password" />
           </div>
           <div className="upBtns text-center">
             <button type="submit" className="btn-default">
