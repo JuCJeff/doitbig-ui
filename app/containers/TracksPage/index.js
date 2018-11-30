bb246b95bf9f96e978b00d20487a4e445247f0d1
Luqianyi Ye <lye25@wisc.edu>
2018-11-30 14:32:48 -0600

    Update the 404 page

--- app/containers/TracksPage/index.js
+++ app/containers/TracksPage/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import TrackImage1 from '../../images/STS-Banner.jpg';
 import NavigationBar from '../../components/NavigationBar/index';
-import './style.css';
+import Tracks from './trackCard';
 import './iconstyle.css';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -10,41 +9,9 @@ export default class TracksPage extends React.PureComponent {
     return (
       <div className="body">
         <NavigationBar />
-        <div className="banner">
-          <img src={TrackImage1} width="100%" alt="banner" />
-        </div>
-
-        <div>
-          <meta charSet="utf-8" />
-          <title>Glowing Icon Effects</title>
-          <link rel="stylesheet" href="style.css" />
-          <link
-            href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-            rel="stylesheet"
-          />
-          <ul className="icons">
-            <li>
-              <a href="/course">
-                <i className="fa fa-graduation-cap" aria-hidden="true" />
-              </a>
-            </li>
-            <li>
-              <a href="/course">
-                <i className="fa fa-graduation-cap" aria-hidden="true" />
-              </a>
-            </li>
-            <li>
-              <a href="/course">
-                <i className="fa fa-graduation-cap" aria-hidden="true" />
-              </a>
-            </li>
-            <li>
-              <a href="/course">
-                <i className="fa fa-graduation-cap" aria-hidden="true" />
-              </a>
-            </li>
-          </ul>
-        </div>
+        <Tracks />
+        <Tracks />
+        <Tracks />
       </div>
     );
   }
