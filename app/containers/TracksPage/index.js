f9a53522ffa9d25a5e9bf24bb47a12f42310e125
Yu Hao <hao43@wisc.edu>
2018-11-30 18:46:24 -0600

    changed the images on tracks page to make it also hard coded

--- app/containers/TracksPage/index.js
+++ app/containers/TracksPage/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import School from '@material-ui/icons/School';
 import Navigation from '../../components/NavigationBar/index';
-import TrackImage1 from '../../images/STS-Banner.jpg';
+import TrackImage1 from '../../images/track1.png';
+import TrackImage2 from '../../images/track2.png';
+import TrackImage3 from '../../images/track3.jpg';
 import './style.css';
 
 /* eslint-disable react/prefer-stateless-function */
@@ -15,7 +17,7 @@ export default class TracksPage extends React.PureComponent {
           <img src={TrackImage1} width="100%" alt="banner" />
         </div>
 
-        <h1>Web Development Track</h1>
+        <h1>Web Developer Track</h1>
         <ul>
           <li>
             <Button variant="fab">
@@ -45,10 +47,43 @@ export default class TracksPage extends React.PureComponent {
         </ul>
 
         <div className="banner">
-          <img src={TrackImage1} width="100%" alt="banner" />
+          <img src={TrackImage2} width="100%" alt="banner" />
         </div>
 
-        <h1>Design Track</h1>
+        <h1>Designer Track</h1>
+        <ul>
+          <li>
+            <Button variant="fab">
+              <School />
+            </Button>
+          </li>
+          <li>
+            <Button variant="fab">
+              <School />
+            </Button>
+          </li>
+          <li>
+            <Button variant="fab">
+              <School />
+            </Button>
+          </li>
+          <li>
+            <Button variant="fab">
+              <School />
+            </Button>
+          </li>
+          <li>
+            <Button variant="fab">
+              <School />
+            </Button>
+          </li>
+        </ul>
+
+        <div className="banner">
+          <img src={TrackImage3} width="100%" alt="banner" />
+        </div>
+
+        <h1>Data Expert Track</h1>
         <ul>
           <li>
             <Button variant="fab">
