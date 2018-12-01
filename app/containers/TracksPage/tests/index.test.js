cafe8b77e960833e0fafdd79e4200f3dabb0bf59
cmdcarini <24967694+cmdcarini@users.noreply.github.com>
2018-11-30 19:12:02 -0600

    Refactored to fix tests.

--- app/containers/TracksPage/tests/index.test.js
+++ app/containers/TracksPage/tests/index.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Button from '@material-ui/core/Button';
-import School from '@material-ui/icons/School';
-import TrackImage1 from '../../../images/STS-Banner.jpg';
+import NavigationBar from '../../../components/NavigationBar/index';
+import Tracks from '../trackCard';
 import TracksPage from '../index';
 
 describe('<TracksPage />', () => {
@@ -11,71 +10,10 @@ describe('<TracksPage />', () => {
     expect(
       renderedComponent.contains(
         <div className="body">
-          <div className="banner">
-            <img src={TrackImage1} width="100%" alt="banner" />
-          </div>
-
-          <h1>Web Development Track</h1>
-          <ul>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-          </ul>
-
-          <div className="banner">
-            <img src={TrackImage1} width="100%" alt="banner" />
-          </div>
-
-          <h1>Design Track</h1>
-          <ul>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-            <li>
-              <Button variant="fab">
-                <School />
-              </Button>
-            </li>
-          </ul>
+          <NavigationBar />
+          <Tracks />
+          <Tracks />
+          <Tracks />
         </div>,
       ),
     ).toEqual(true);
